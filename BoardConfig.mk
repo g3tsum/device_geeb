06e46dbcc1b1243682efd2ba40f1cc3837b6ed6a
houstonn <houstonn@gmail.com>
2013-03-29 03:53:54 +0000

    flag changes by me and media profile enhancements by _Epic

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,9 @@
 #
 
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64  
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp  
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp  
-TARGET_EXTRA_CFLAGS := -mtune=cortex-a15 -mcpu=cortex-a15 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a9 -mfloat-abi=softfp  
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a9 -mfloat-abi=softfp  
+TARGET_EXTRA_CFLAGS := -mtune=cortex-a9 -mcpu=cortex-a9 
 
 TARGET_CPU_VARIANT := krait
 TARGET_CPU_ABI := armeabi-v7a
@@ -25,7 +25,6 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
