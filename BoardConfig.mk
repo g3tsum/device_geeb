9c762298de8174e897a80c4f796a9bcbd7171fea
houstonn <houstonn@gmail.com>
2013-03-30 20:34:38 +0000

    changed flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,9 @@
 #
 
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64  
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a9 -mfloat-abi=softfp  
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a9 -mfloat-abi=softfp  
-TARGET_EXTRA_CFLAGS := -mtune=cortex-a9 -mcpu=cortex-a9 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp  
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp  
+TARGET_EXTRA_CFLAGS := -mtune=cortex-a15 -mcpu=cortex-a15
 
 TARGET_CPU_VARIANT := krait
 TARGET_CPU_ABI := armeabi-v7a
@@ -110,6 +110,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/lge/geeb
 
 BOARD_CHARGER_ENABLE_SUSPEND := true
 TARGET_QCOM_DISPLAY_VARIANT := caf
+BOARD_HAVE_OLD_ION_API := true
 
 -include vendor/houstonn/config/BoardConfigVendor.mk
 
